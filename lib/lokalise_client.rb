f619c76bde2cfff1b02a23395b1e2fd559382722
wichru <maciej.wicher@netguru.co>
2021-09-26 20:11:11 +0200

    Allow to create new translation keys

--- lib/lokalise_client.rb
+++ lib/lokalise_client.rb
@@ -1,9 +1,34 @@
 # frozen_string_literal: true
 
 module LokaliseClient
-  module_function
+  extend self
+
+  PROJECT_ID = Rails.application.credentials.lokalise[:project_id]
+
+  def create_keys(key_params)
+    client.create_keys(PROJECT_ID, key_params)
+  end
+
+  private
 
   def client
     @client ||= Lokalise.client(Rails.application.credentials.lokalise[:api_key])
   end
+  #
+  # def key_params
+  #   {
+  #     keys: [
+  #       {
+  #         key_name: key_name,
+  #         platforms: %w[ios web android other],
+  #         translations: [
+  #           {
+  #             language_iso: 'en',
+  #             translation: 'Welcome',
+  #           },
+  #         ],
+  #       },
+  #     ],
+  #   }
+  # end
 end
