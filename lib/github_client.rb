cca0edc433383ced4a8ea149d0e4cf279bc83167
wichru <maciej.wicher@netguru.co>
2021-09-26 13:22:25 +0200

    Update github_client.rb

--- lib/github_client.rb
+++ lib/github_client.rb
@@ -1,7 +1,19 @@
 # frozen_string_literal: true
 
 module GithubClient
-  module_function
+  extend self
+
+  def fetch_file(file_url)
+    client.get(file_url)
+  rescue Octokit::NotFound
+    false
+  end
+
+  def create_pull_request(repo_name, branch_name)
+    client.create_pull_request(repo_name, 'develop', branch_name, "cerate-#{branch_name}")
+  end
+
+  private
 
   def client
     @client ||= Octokit::Client.new(access_token: Rails.application.credentials.github[:access_token])
